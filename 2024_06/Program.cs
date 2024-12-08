e3df3f0c21d0acc78c839235f80414f61843d0ee
Oscaruzzo <oscar.lazzarino@gmail.com>
2024-12-08 15:55:11 +0100

    Fase 2

--- 2024_06/Program.cs
+++ 2024_06/Program.cs
@@ -4,6 +4,14 @@
 	{
 		private const int w = 130;
 		private const int h = 130;
+
+		static int[] xDirs = [0, +1, 0, -1];
+		static int[] yDirs = [-1, 0, +1, 0];
+
+		private const int WALL = 1000;
+		private const int SPACE = 0;
+		private const int SEEN = 2000;
+
 		protected static void WriteAt(char s, int x, int y) {
 			try {
 				Console.SetCursorPosition(x, y);
@@ -13,8 +21,7 @@
 				Console.WriteLine(e.Message);
 			}
 		}
-		static void printMatrix(char[,] data, int cx, int cy, int currentDir) {
-			//Console.Clear();
+		protected static void PrintMatrix(int[,] data, int cx, int cy, int currentDir) {
 			for (int y = 0; y < h; y++) {
 				for (int x = 0; x < w; x++) {
 					if (x == cx && y == cy) {
@@ -32,16 +39,70 @@
 							WriteAt('←', x, y);
 							break;
 						}
-					} else {
-						WriteAt(data[x, y], x, y);
+					} else if (data[x, y] == WALL) {
+						WriteAt('#', x, y);
+					} else if (data[x, y] == SPACE) {
+						WriteAt('·', x, y);
+					} else { // SEEN
+						WriteAt('o', x, y);
 					}
 				}
-				Console.WriteLine();
 			}
 			Console.WriteLine();
 		}
+
+		protected static int Walk(int[,] data, int startX, int startY) {
+			int x = startX, y = startY;
+			int currentDir = 0;
+			int len = 0;
+			bool done = false;
+			while (!done) {
+				// check current position
+				if (data[x, y] == SPACE) {
+					len++;
+				}
+				if (0 != (data[x, y] & 1 << currentDir)) {
+					// already been here in this direction! it's a loop
+					return -1;
+				}
+				data[x, y] |= 1 << currentDir;
+
+				//PrintMatrix(data, x, y, currentDir);
+				int newX, newY;
+				do {
+					newX = x + xDirs[currentDir];
+					newY = y + yDirs[currentDir];
+					// is out of matrix?
+					if (newX < 0 || newY < 0 || newX >= w || newY >= h) {
+						done = true;
+						break;
+					}
+					// is against wall?
+					else if (data[newX, newY] == WALL) {
+						currentDir = (currentDir + 1) % 4;
+					}
+
+					// can go
+					else {
+						break;
+					}
+				} while (true);
+				x = newX;
+				y = newY;
+			}
+
+			return len;
+		}
+
+		protected static void ResetData(int[,] data) {
+			for (int x = 0; x < w; x++)
+				for (int y = 0; y < h; y++)
+					if (data[x, y] != WALL)
+						data[x, y] = SPACE;
+		}
+
 		static void Main(string[] args) {
-			char[,] data = new char[w, h];
+			int[,] data = new int[w, h];
 			int startX = -1, startY = -1;
 
 			{ // read input
@@ -49,12 +110,14 @@
 				foreach (var line in File.ReadLines("input.txt")) {
 					int x = 0;
 					foreach (var c in line) {
-						if (c == '^') {
-							data[x, y] = '.';
+						if (c == '#') {
+							data[x, y] = WALL;
+						} else if (c == '.') {
+							data[x, y] = SPACE;
+						} else if (c == '^') {
+							data[x, y] = SPACE;
 							startX = x;
 							startY = y;
-						} else {
-							data[x, y] = c;
 						}
 						x++;
 					}
@@ -63,44 +126,24 @@
 			}
 
 			{ // part 1
-				int[] xDirs = { 0, +1, 0, -1 };
-				int[] yDirs = { -1, 0, +1, 0 };
+				int walkLen = Walk(data, startX, startY);
+				Console.WriteLine(walkLen);
+			}
 
-				int x = startX, y = startY;
-				int currentDir = 0;
-				int tot = 0;
-				bool done = false;
-				while (!done) {
-					// check current position
-					if (data[x, y] == '.') {
-						tot++;
-						data[x, y] = 'X';
+			{ // part 2
+				int nLoops = 0;
+				for (int x = 0; x < w; x++) {
+					for (int y = 0; y < h; y++) {
+						ResetData(data);
+						if (data[x, y] != WALL && (x != startX || y != startY)) {
+							data[x, y] = WALL;
+							if (Walk(data, startX, startY) == -1)
+								nLoops++;
+							data[x, y] = SPACE;
+						}
 					}
-					//printMatrix(data, x, y, currentDir);
-					int newX, newY;
-					do {
-						newX = x + xDirs[currentDir];
-						newY = y + yDirs[currentDir];
-						// is out of matrix?
-						if (newX < 0 || newY < 0 || newX >= w || newY >= h) {
-							done = true;
-							break;
-						}
-						// is against wall?
-						else if (data[newX, newY] == '#') {
-							currentDir = (currentDir + 1) % 4;
-						}
-
-						// can go
-						else {
-							break;
-						}
-					} while (true);
-					x = newX;
-					y = newY;
 				}
-
-				Console.WriteLine(tot);
+				Console.WriteLine(nLoops);
 			}
 		}
 	}
